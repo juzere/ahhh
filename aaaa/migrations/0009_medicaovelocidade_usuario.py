fc1b52f1871093444ea33b7d2eda39fe5c84cc19
juzere <jullia.azeredo@acad.ufsm.br>
2024-02-06 08:51:16 -0300

    final

--- aaaa/migrations/0009_medicaovelocidade_usuario.py
+++ aaaa/migrations/0009_medicaovelocidade_usuario.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='medicaovelocidade',
             name='usuario',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=3, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
--- tes_dispositivo.py
+++ tes_dispositivo.py
@@ -1,12 +1,22 @@
 import requests
 
+# URL da API para a qual você está enviando os dados
 url = "http://127.0.0.1:8000/api/receber/"
-dados = {"velocidade": 60.2}
 
+# Atualize esta seção para incluir o 'usuario_id' junto aos dados de velocidade
+# Substitua '1' pelo ID do usuário apropriado que você deseja simular
+dados = {
+    "velocidade": 60.2,
+    "usuario_id": 5
+}
+
+# Fazendo a requisição POST para a API
 response = requests.post(url, json=dados)
 
+# Verificando a resposta
 if response.status_code == 200:
     try:
+        # Tentando extrair e imprimir a resposta JSON
         data = response.json()
         print("Response:", data)
     except Exception as e:
@@ -14,3 +24,4 @@ if response.status_code == 200:
 else:
     print("Status Code:", response.status_code)
     print("Response:", response.text)
+
