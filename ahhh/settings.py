15c5bb92629b9ebb7641ba842dc84919cb8dbf53
juzere <jullia.azeredo@acad.ufsm.br>
2024-02-16 10:31:46 -0300

    database3

--- ahhh/settings.py
+++ ahhh/settings.py
@@ -75,9 +75,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ahhh.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 DATABASES = {
     'default': {
