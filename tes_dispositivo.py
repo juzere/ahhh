49e1586d91350e0af9082a8250265ebb203c41cf
juzere <jullia.azeredo@acad.ufsm.br>
2024-02-22 11:43:17 -0300

    .

--- tes_dispositivo.py
+++ tes_dispositivo.py
@@ -1,21 +1,22 @@
 import requests
+import random
 
 url = "http://127.0.0.1:8000//api/receber/"
 
-
 usuario_id = 5
 
+# Gerar um número aleatório entre 0 e 100
+velocidade = round(random.uniform(0, 100), 1)
+
 dados = {
-    "velocidade": 55.2,
+    "velocidade": velocidade,
     "usuario_id": usuario_id
 }
-
 
 response = requests.post(url, json=dados)
 
 if response.status_code == 200:
     try:
-   
         data = response.json()
         print("Response:", data)
     except Exception as e:
